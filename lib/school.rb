f81f556a7727485d00687c36620276561d438747
Alec Hall <alecnhall@gmail.com>
2019-08-27 21:21:45 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -5,12 +5,12 @@ class School
   end
   
   def roster 
-    @roster = { }
+    roster = { }
   end
   
   def add_student(students_name, students_grade)
-    @roster[students_grade] = []
-    @roster[students_grade] << students_name
+    roster[students_grade] = []
+    roster[students_grade] << students_name
   end
   
 end
