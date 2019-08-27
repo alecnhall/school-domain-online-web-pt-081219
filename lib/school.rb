8a32eec23a67272189d8b3a6a499dddfcf9953e8
Alec Hall <alecnhall@gmail.com>
2019-08-27 21:28:11 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -6,7 +6,7 @@ class School
   end
   
   def add_student(students_name, students_grade)
-    if @roster == {} && students_grade == []
+    if @roster == {}
       @roster[students_grade] = []
       @roster[students_grade] << students_name
     else
